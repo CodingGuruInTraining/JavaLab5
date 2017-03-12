0f56e44fde08e76b3ca39f8b3fd205f6bfc542a7
MF <notsure@compuserv.com>
2017-03-11 21:53:03 -0600

    added exception handlers

--- src/com/mark/adv_problem_1.java
+++ src/com/mark/adv_problem_1.java
@@ -21,20 +21,26 @@ public class adv_problem_1 {
             }
         }
         catch (IOException err) {
-            System.out.println("Unable to access file.");
+            System.out.println("Unable to read/find file.");
         }
     }
 
     public static ArrayList<String[]> readDrinkInfo(BufferedReader buffReader) throws IOException {
-        ArrayList<String[]> all_drinks = new ArrayList<String[]>();
-        String drink_line = buffReader.readLine();
-        while (drink_line != null) {
-            String[] drink_info = drink_line.split(";");
-            all_drinks.add(drink_info);
-            drink_line = buffReader.readLine();
+        try {
+            ArrayList<String[]> all_drinks = new ArrayList<String[]>();
+            String drink_line = buffReader.readLine();
+            while (drink_line != null) {
+                String[] drink_info = drink_line.split(";");
+                all_drinks.add(drink_info);
+                drink_line = buffReader.readLine();
+            }
+            buffReader.close();
+            return all_drinks;
         }
-        buffReader.close();
-        return all_drinks;
+        catch (IOException err) {
+            System.out.println("There is a problem reading the file.");
+            return null;
+        }
     }
 
     public static HashMap askSales(ArrayList<String[]> all_drinks) {
@@ -58,35 +64,39 @@ public class adv_problem_1 {
     }
 
     public static void makeSalesReport(ArrayList<String[]> all_drinks, HashMap salesData) throws IOException {
-        FileWriter writer = new FileWriter("sales-report.txt");
-        BufferedWriter buffWriter = new BufferedWriter(writer);
+        try (BufferedWriter buffWriter = new BufferedWriter(new FileWriter("sales-report.txt"))) {
+//            FileWriter writer = new FileWriter("sales-report.txt");
+//            BufferedWriter buffWriter = new BufferedWriter(writer);
 
-        Date today = new Date();
-        DateFormat formatDate = new SimpleDateFormat("MMMM dd, yyyy");
-        String todayStr = formatDate.format(today);
-        buffWriter.write(String.format("Sales Report for: %s\n", todayStr));
+            Date today = new Date();
+            DateFormat formatDate = new SimpleDateFormat("MMMM dd, yyyy");
+            String todayStr = formatDate.format(today);
+            buffWriter.write(String.format("Sales Report for: %s\n", todayStr));
 
-        for (Integer i = 0; i < all_drinks.size(); i++) {
-            String drink = all_drinks.get(i)[0];
-            int sold = (int) salesData.get(drink);
-            double costPer = Double.parseDouble(all_drinks.get(i)[1]);
-            double cost = costPer * sold;
-            double pricePer = Double.parseDouble(all_drinks.get(i)[2]);
-            double earned = pricePer * sold;
-            double profit = earned - cost;
+            for (Integer i = 0; i < all_drinks.size(); i++) {
+                String drink = all_drinks.get(i)[0];
+                int sold = (int) salesData.get(drink);
+                double costPer = Double.parseDouble(all_drinks.get(i)[1]);
+                double cost = costPer * sold;
+                double pricePer = Double.parseDouble(all_drinks.get(i)[2]);
+                double earned = pricePer * sold;
+                double profit = earned - cost;
 
-            String drinkStr = drink + ":";
-            String soldStr = "Sold " + sold + ",";
-            String costStr = String.format("Expenses $%.2f,", cost);
-            String earnedStr = String.format("Revenue $%.2f,", earned);
-            String profitStr = String.format("Profit $%.2f", profit);
-            buffWriter.write(String.format("%-14s %-2s %-14s %-14s %s\n",
-                    drinkStr, soldStr, costStr, earnedStr, profitStr));
+                String drinkStr = drink + ":";
+                String soldStr = "Sold " + sold + ",";
+                String costStr = String.format("Expenses $%.2f,", cost);
+                String earnedStr = String.format("Revenue $%.2f,", earned);
+                String profitStr = String.format("Profit $%.2f", profit);
+                buffWriter.write(String.format("%-14s %-2s %-14s %-14s %s\n",
+                        drinkStr, soldStr, costStr, earnedStr, profitStr));
+            }
+            buffWriter.close();
+            System.out.println("Report successfully created.");
         }
-        buffWriter.close();
-        System.out.println("Report successfully created.");
+        catch (IOException err) {
+            System.out.println("Unable to write to file.");
+        }
     }
-
 }
 
 //helpful sites:
