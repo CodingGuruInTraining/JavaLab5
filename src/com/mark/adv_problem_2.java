a863cad73c0163f79b7c8bfeecf5df70c495839c
MF <notsure@compuserv.com>
2017-03-12 18:02:38 -0500

    added comments

--- src/com/mark/adv_problem_2.java
+++ src/com/mark/adv_problem_2.java
@@ -7,25 +7,32 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
- * This program tests...
+ * This program tests creating a new directory and adding a
+ * new file to it.
  */
 public class adv_problem_2 {
     public static void main(String[] args) {
+        // Exception handler.
         try {
-
+            // Captures the path of the current location of the program.
             String fullPath = Paths.get(".").toAbsolutePath().normalize().toString();
+            // Creates less wordy variable for adding the appropriate separator
+            // based on the system in use (hopefully).
             String sep = File.separator;
+            // Creates a new File object.
             Path newFile = Paths.get(fullPath + sep + "data" + sep + "sample.txt");
+            // Creates a new directory if one does not exist.
             Files.createDirectories(newFile.getParent());
+            // Adds the File object to the new directory.
             Files.createFile(newFile);
 
         }
         catch (IOException err) {
+            // Error occurs when trying to create a file with the same name.
             System.out.println("That file already exists.");
         }
     }
 }
-
 
 //helpful sites:
 //        http://stackoverflow.com/questions/2833853/create-whole-path-automatically-when-writing-to-a-new-file
