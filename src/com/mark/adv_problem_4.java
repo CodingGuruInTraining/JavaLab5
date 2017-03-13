ebae25553a903e7c7a8b8fc2d328d209cd73742c
MF <notsure@compuserv.com>
2017-03-12 22:52:07 -0500

    added write statements added comments

--- src/com/mark/adv_problem_4.java
+++ src/com/mark/adv_problem_4.java
@@ -11,22 +11,31 @@ import java.io.IOException;
  */
 public class adv_problem_4 {
     public static void main(String[] args) {
+        // Exception handler with BufferedWriter object created inside it.
         try (BufferedWriter buffWriter = new BufferedWriter(new FileWriter("mySystem.txt"))) {
+            // Retrieves certain System Properties and stores in strings.
             String osName = System.getProperty("os.name");
             String osVersion = System.getProperty("os.version");
             String osArchitecture = System.getProperty("os.arch");
             String userName = System.getProperty("user.name");
             String line = System.getProperty("line.separator");
 
-            System.out.println(osName);
-            System.out.println(osVersion);
-            System.out.println(osArchitecture);
-            System.out.println(userName);
+            // Writes properties to a file.
+            buffWriter.write("Some of the current system's properties include:" + line);
+            buffWriter.write("OS Name: " + osName + line);
+            buffWriter.write("OS Version: " + osVersion + line);
+            buffWriter.write("OS Architecture: " + osArchitecture + line);
+            buffWriter.write("User Name: " + userName + line);
+
+            // Closes writer object.
             buffWriter.close();
         }
         catch (IOException err) {
+            // Exception for problems with the file and/or location.
             System.out.println("That file already exists.");
         }
-
     }
 }
+
+//helpful site:
+//        http://www.ibm.com/support/knowledgecenter/ssw_i5_54/rzaha/sysprop2.htm
